6448d31a95f42abde8339a6e89abfe04bfb7483c
jihad <abbdurahmanj@gmail.com>
2020-05-26 09:44:39 +0700

    function pass by value parameter

---
+++ function_pass_by_value/main.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+void addNum(int number,int adder){
+    number=number+adder;
+    cout<<"Value on addNum function is ";
+    cout<<number<<endl;
+}
+
+int main(){
+
+    int number=10;
+    cout<<"Initial Value is ";
+    cout<<number<<endl;
+
+    // change value locally
+    addNum(number,10);
+
+    // value of 'number variable' doesn't change
+    cout<<"Value on main function still ";
+    cout<<number<<endl;
+
+    return 0;
+
+}
