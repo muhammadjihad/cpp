6468fd7d4a2289bcaac251dc918cd4fd4afb809a
jihad <abbdurahmanj@gmail.com>
2020-06-01 21:27:59 +0800

    matrices upper triangle matrix

---
+++ data_structure/matrices/sparse_matrix/main.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+struct SparseMatrix{
+    int *X;
+    int *Y;
+    int Val;
+};
+
+
+int main(){
+
+    struct SparseMatrix *M;
+    M[0]={3,2,4};
+
+    return 0;
+}
--- data_structure/matrices/upper_triangle_matrix/main.cpp
+++ data_structure/matrices/upper_triangle_matrix/main.cpp
@@ -3,12 +3,78 @@
 struct UpperTriangleMatrix{
 
     int size;
-    int *A;
+    int *M;
 
+};
+
+int MatrixToArrayLength(int size){
+    return (size*(size+1))/2;
+}
+
+int Index(int size,int i,int j){
+    return ((i-1)*size-(i-2)*(i-1)/2)+(j-i);
+}
+
+void Display(struct UpperTriangleMatrix m){
+
+    int i;
+    int arrSize=MatrixToArrayLength(m.size);
+    for(i=0;i<arrSize;i++){
+        printf("%d\n",m.M[i]);
+    }
+
+}
+
+struct UpperTriangleMatrix Create(int size){
+
+    struct UpperTriangleMatrix m;
+    int arrSize=MatrixToArrayLength(size);
+    m.M=new int[arrSize];
+    m.size=size;
+    int i;
+    for(i=0;i<arrSize;i++){
+        m.M[i]=0;
+    }
+
+    return m;
+
+}
+
+void Set(struct UpperTriangleMatrix *m,int i, int j, int val){
+    if(j>=i){
+        int index=Index(m->size,i,j);
+        printf("stored at index - %d",index);
+        m->M[index]=val;
+    }
+}
+
+int Get(struct UpperTriangleMatrix m,int i,int j){
+    if(j>=i){
+        int index=Index(m.size,i,j);
+        return m.M[index];
+    }
+    return 0;
 }
 
 int main(){
 
+    struct UpperTriangleMatrix m=Create(4);
+
+    Display(m);
+
+    Set(&m,2,2,10);
+
+    Set(&m,2,4,12);
+
+    Set(&m,1,1,9);
+    
+    printf("\n");
+
+    Display(m);
+
+    printf("\n");
+
+    printf("Get element at (1,1) - %d\n",Get(m,1,1));
 
     return 0;
 }
